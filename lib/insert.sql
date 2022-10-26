53fd2e75ad5b5232d328973e518e25a0904545e6
Victoria Parry <vlparry@gmail.com>
2022-10-26 13:19:11 -0400

    commit tree 4b3a99604bcad361fd6e111e06813f9197ebfbe2

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,'Mr. Chocolate', 23, 'M', 'Black', 'Happy', TRUE);
+INSERT INTO bears VALUES (2,'Rowdy', 1, 'F', 'Black', 'Crazy', TRUE);
+INSERT INTO bears VALUES (3,'Tabitha', 12, 'F', 'Brown', 'Happy', FALSE);
+INSERT INTO bears VALUES (4,'Sergeant Brown', 18, 'M', 'Brown', 'Calm', TRUE);
+INSERT INTO bears VALUES (5,'Melissa', 4, 'F', 'Black', 'Calm', FALSE);
+INSERT INTO bears VALUES (6,'Grinch', 26, 'M', 'Black', 'Mean', TRUE);
+INSERT INTO bears VALUES (7,'Wendy', 6, 'F', 'Brown', 'Calm', TRUE);
+INSERT INTO bears VALUES (8,NULL, 1, 'F', 'Brown', 'Silly', TRUE);
